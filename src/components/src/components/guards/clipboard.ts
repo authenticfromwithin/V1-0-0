8da9d4752452ccdee97a8e0c9b5b25785d467916
authenticfromwithin <Authenticfromwithin@gmail.com>
2025-08-16 17:43:56 +0200

    Delete src/components/src/components/guards/clipboard.ts

--- src/components/src/components/guards/clipboard.ts
+++
@@ -1,8 +0,0 @@
-// shim: keep legacy import "../guards/clipboard" working from components/journal/*
-export * from '../../guards/clipboard';
-// preserve default export too, if the module has one
-// (harmless if it doesn't)
-// @ts-ignore
-import def from '../../guards/clipboard';
-// @ts-ignore
-export default def;
