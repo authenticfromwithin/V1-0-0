b2abe183661dea44c4624a5948bcc47eea57beac
authenticfromwithin <Authenticfromwithin@gmail.com>
2025-08-17 11:50:26 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles/global.css";
+import "./styles/globals.css";
 import "./styles/fonts.css";
 
 const container = document.getElementById("root")!;
