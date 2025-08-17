40ab9c2b02dafcdbea1d6a2c85ed9786125c1ea5
authenticfromwithin <Authenticfromwithin@gmail.com>
2025-08-17 10:42:47 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./styles/global.css";
+import "./styles/fonts.css";
 
-const el = document.getElementById('root')!;
-createRoot(el).render(
+const container = document.getElementById("root")!;
+createRoot(container).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
