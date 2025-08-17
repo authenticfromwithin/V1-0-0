e771b5996bfc06f23057b5dff1d57f24ca9b278f
authenticfromwithin <Authenticfromwithin@gmail.com>
2025-08-17 10:48:35 +0200

    Create NotFound.tsx

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function NotFound() {
+  return (
+    <main className="page">
+      <h2>Not Found</h2>
+      <p>The path you seek is not here.</p>
+    </main>
+  );
+}
